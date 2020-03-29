b5afa9e544bf335c2115c7d5efb6cc2dc5a82f41
Taulant Ibraj <tibraj94@gmail.com>
2020-03-29 16:20:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,6 @@
 # Write your code here.
 katz_deli = []
 def line(array)
-  if array != []
-    array.each do |element|
-    puts "The line is currently: #{array.index + 1}. #{element}"
-
 end
 
 def take_a_number
