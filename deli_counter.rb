ede85803079737e866b80900bd9c440696fb0eb3
Taulant Ibraj <tibraj94@gmail.com>
2020-03-29 16:59:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    array.each.with_index(2) do |element, i|
+    array.each.with_index(1) do |element, i|
     puts "The line is currently: #{i}. #{element}"
       end
     end
