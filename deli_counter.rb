eb46e41185f304737a871bd7ea8ada100e9fdc19
Taulant Ibraj <tibraj94@gmail.com>
2020-03-29 16:24:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,5 @@ katz_deli = []
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
-  elsif array.length != 0
-      array.each do |element|
-      puts "The line is currently: #{array.index + 1}. #{element}"
     end
 end
